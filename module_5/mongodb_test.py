39758127e6111b4a72c01b29762bf7c4b2e21817
Jason <private@petstergaming.com>
2022-01-09 17:45:55 -0500

    Create mongodb_test.py

---
+++ module_5/mongodb_test.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.wfbdf.mongodb.net/pytech?retryWrites=true&w=majority"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+print(db.list_collection_names())   
