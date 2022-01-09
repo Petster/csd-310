77faf15fa315af3d6dc886a603d4165a7faa8db5
Jason <private@petstergaming.com>
2022-01-09 18:14:20 -0500

    Create pytech_insert.py

---
+++ module_5/pytech_insert.py
@@ -0,0 +1,23 @@
+# pytech_insert
+# Jason Palmeri
+# 1/9/2022
+
+from pymongo import MongoClient
+
+url = "mongodb+srv://admin:admin@cluster0.wfbdf.mongodb.net/pytech"
+
+client = MongoClient(url)
+
+db = client.pytech
+
+student1 = {
+
+}
+
+student2 = {
+
+}
+
+student3 = {
+    
+}
