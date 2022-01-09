82f88f4e6d51959707efdb4135bd1055a5faab65
Jason Palmeri <jpalmeri238@students.ccsnh.edu>
2022-01-09 18:21:03 -0500

    Update pytech_insert.py

--- module_5/pytech_insert.py
+++ module_5/pytech_insert.py
@@ -11,13 +11,92 @@ client = MongoClient(url)
 db = client.pytech
 
 student1 = {
-
+    "student_id": "1007",
+    "first_name": "John",
+    "last_name": "Doe",
+    "enrollments": [
+        {
+            "term": "Summer",
+            "gpa": "3.4",
+            "start_date": "January 10, 2022",
+            "end_date": "June 14, 2022",
+            "courses": [
+                {
+                    "course_id": "CSD310",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD320",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
 
 student2 = {
-
+    "student_id": "1008",
+    "first_name": "Jimmy",
+    "last_name": "Doe",
+    "enrollments": [
+        {
+            "term": "Summer",
+            "gpa": "3.4",
+            "start_date": "January 10, 2022",
+            "end_date": "June 14, 2022",
+            "courses": [
+                {
+                    "course_id": "CSD330",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD340",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
 
 student3 = {
-    
+    "student_id": "1009",
+    "first_name": "Josh",
+    "last_name": "Doe",
+    "enrollments": [
+        {
+            "term": "Summer",
+            "gpa": "3.4",
+            "start_date": "January 10, 2022",
+            "end_date": "June 14, 2022",
+            "courses": [
+                {
+                    "course_id": "CSD350",
+                    "description": "Database Development and Use",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                },
+                {
+                    "course_id": "CSD360",
+                    "description": "Programming with Java",
+                    "instructor": "Professor Krasso",
+                    "grade": "A+"
+                }
+            ]
+        }
+    ]
 }
+
+students = db.students
+
+print("Inserting...")
+student1_student_id = students.insert_one(student1).inserted_id
+
+print("  Inserted student record Student1 into the students collection with document_id " + str(student1_student_id))
