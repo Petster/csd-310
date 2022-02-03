3f074ba97437551d47d54c124c128911e062bd8c
Jason Palmeri <jpalmeri238@students.ccsnh.edu>
2022-02-02 23:07:14 -0500

    fixed commas

--- module_10/whatabook_init.sql
+++ module_10/whatabook_init.sql
@@ -1,17 +1,12 @@
-CREATE TABLE user(
-   user_id INT NOT NULL AUTO_INCREMENT,
-   first_name VARCHAR(75) NOT NULL,
-   last_name VARCHAR(75) NOT NULL
-   PRIMARY KEY (user_id)
-);
+DROP TABLE IF EXISTS store;
+DROP TABLE IF EXISTS book;
+DROP TABLE IF EXISTS wishlist;
+DROP TABLE IF EXISTS user;
 
-CREATE TABLE wishlist(
-   wishlist_id INT NOT NULL AUTO_INCREMENT,
-   user_id INT NOT NULL,
-   book_id INT NOT NULL,
-   PRIMARY KEY (wishlist_id),
-   FOREIGN KEY (user_id) REFERENCES user(user_id),
-   FOREIGN KEY (book_id) REFERENCES book(book_id)
+CREATE TABLE store(
+   store_id INT NOT NULL AUTO_INCREMENT,
+   locale VARCHAR(500) NOT NULL,
+   PRIMARY KEY(store_id)
 );
 
 CREATE TABLE book(
@@ -19,13 +14,23 @@ CREATE TABLE book(
    book_name VARCHAR(200) NOT NULL,
    details VARCHAR(500),
    author VARCHAR(200) NOT NULL,
-   PRIMARY KEY (book_id),
+   PRIMARY KEY(book_id)
 );
 
-CREATE TABLE store(
-   store_id INT NOT NULL AUTO_INCREMENT,
-   locale VARCHAR(500) NOT NULL,
-   PRIMARY KEY (store_id)
+CREATE TABLE user(
+   user_id INT NOT NULL AUTO_INCREMENT,
+   first_name VARCHAR(75) NOT NULL,
+   last_name VARCHAR(75) NOT NULL,
+   PRIMARY KEY(user_id)
+);
+
+CREATE TABLE wishlist(
+   wishlist_id INT NOT NULL AUTO_INCREMENT,
+   user_id INT NOT NULL,
+   book_id INT NOT NULL,
+   PRIMARY KEY(wishlist_id),
+   FOREIGN KEY (user_id) REFERENCES user(user_id),
+   FOREIGN KEY (book_id) REFERENCES book(book_id)
 );
 
 INSERT INTO store(locale) VALUES('123 Northeastern Road, Brookline, NH 03033');
