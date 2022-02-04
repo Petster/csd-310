d00d10d5c832a229ea5dc5be53861aa6a327b2c9
Jason Palmeri <jpalmeri238@students.ccsnh.edu>
2022-02-03 23:19:35 -0500

    Tried to do a GUI for WhatABook, gave up, might try again. finished WhatABook CLI

--- module_12/main.py
+++ module_12/main.py
@@ -2,25 +2,34 @@ import tkinter as Tk
 from tkinter import ttk
 from tkinter import font
 
-#init tkinter
-root = Tk.Tk()
-root.title("WhatABook Online")
-root.geometry('1280x720')
-
-def createMenuFrame():
-    menuFrame = ttk.Frame(root)
+def createMenuFrame(container):
+    menuFrame = ttk.Frame(container)
     buttonFont = font.Font(size=20)
-    labelOne = Tk.Button(menuFrame, text="View Books", font=buttonFont).grid(row=1, column=0, columnspan=2)
-    labelTwo = Tk.Button(menuFrame, text="View Store Locations", font=buttonFont).grid(row=1, column=3)
-    labelThree = Tk.Button(menuFrame, text="My Account", bg='blue', fg='white', font=buttonFont).grid(row=2)
-    labelFour = Tk.Button(menuFrame, text="Exit", font=buttonFont, bg='red', fg='white', command=root.destroy).grid(row=3)
-    menuFrame.place(x=0, y=0)
+    labelOne = Tk.Button(menuFrame, text="View Books", font=buttonFont, command= lambda: viewBooks(root, menuFrame)).grid(row=0, column=0)
+    labelTwo = Tk.Button(menuFrame, text="View Store Locations", font=buttonFont).grid(row=0, column=1)
+    labelThree = Tk.Button(menuFrame, text="My Account", bg='blue', fg='white', font=buttonFont).place(x=10,y=182,width=570,height=72)
+    labelFour = Tk.Button(menuFrame, text="Exit", font=buttonFont, bg='red', fg='white', command=root.destroy).place(x=0,y=428,width=600,height=72)
     return menuFrame
 
 def clearWindow(frame):
     frame.destroy()
 
-createMenuFrame()
+def viewBooks(root, menuFrame):
+    clearWindow(menuFrame)
+    viewBookFrame = ttk.Frame(root)
+    buttonFont = font.Font(size=20)
+    labelFour = Tk.Button(viewBookFrame, text="Back", font=buttonFont, bg='red', fg='white', command=lambda: createMenuFrame(root)).grid(row=4, columnspan=4)
+    viewBookFrame.pack()
+
+
+#init tkinter
+root = Tk.Tk()
+root.title("WhatABook Online")
+root.geometry('600x500')
+
+start = createMenuFrame(root).grid(column=1, row=0)
+
+
 
 root.resizable(False, False) 
 root.mainloop()
